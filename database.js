14666421801632ed52d00982736b16e6510aafd1
Mahdi Karimi <mhdi_kr@outlook.com>
2020-07-25 09:18:56 +0430

    bug fixed, now people don't carry their own counter to the other chats

--- database.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize} = require('sequelize');
-const DB_NAME = 'postgres'
-const DB_PASSWORD = 'root'
-const DB_USERNAME = 'postgres'
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
-}).authenticate()
-
--- dataservice.js
+++ dataservice.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = import('fs')
 var usrFileName = "./users.json";
 
 var users = {};
@@ -12,19 +12,19 @@ function loadUsers() {
 }
 
 function saveUsers() {
-	if(!fileLocked){
-		fileLocked = true;
-		var json = JSON.stringify(users);
-		fs.writeFile(usrFileName, json, 'utf8', function (err) {
-			if (err) throw err;
-			fileLocked = false;
-		})
-	}
+    if(!fileLocked){
+        fileLocked = true;
+        var json = JSON.stringify(users);
+        fs.writeFile(usrFileName, json, 'utf8', function (err) {
+            if (err) throw err;
+            fileLocked = false;
+        })
+    }
 }
 
 function registerUser(msg) {
     var uid = msg.chat.id;
-    var usr = {enabled: true, data: {from: msg.from, chat: msg.chat}, counter:{ value:0, interval:6, activeMessageId:0 }};
+    var usr = {enabled: true, data: {from: msg.from, chat: msg.chat}, counter:{value:0,interval:6,activeMessageId:0}};
     users[uid] = usr;
     saveUsers();
 }
@@ -46,93 +46,30 @@ function getMetaData(uid, key) {
     return users[uid].data[key];
 }
 
-function assertCounter(uid) {
-    if(users[uid]) {
-        if(users[uid].counter) {
-            if(users[uid].counter) {
-                if("value" in users[uid].counter) {
-                    if("interval" in users[uid].counter){
-                        if("activeMessageId" in users[uid].counter){
-                            return true;
-                        } else {
-                            users[uid].counter.activeMessageId = -1;
-                        }
-                    } else {
-                        users[uid].counter.interval = 6;
-                    }
-                }
-                else {
-                    users[uid].counter.value = 0;
-                }
-            }
-            else {
-                users[uid].counter = {};
-                users[uid].counter.value = 0;
-                users[uid].counter.interval = 6;
-                users[uid].counter.activeMessageId = -1;
-                saveUsers();
-            }
-        }
-        else {
-            users[uid].counter = {};
-            if(users[uid].count) {//old counter detected, migrate count
-                users[uid].counter = {value: users[uid].count};
-                delete users[uid].count;
-            }
-            else {
-                users[uid].counter = {};
-                users[uid].counter.value = 0;
-            }
-            saveUsers();
-        }
-    }
-    else {
-        //console.log("[ERROR] User ID", uid, "does not exist in database");
-        var usr = {enabled: true, data: {from: undefined, chat: undefined, error: "user was not initialized properly"}, counter: {"0": {"value": 1}}};
-        users[uid] = usr;
-        saveUsers();
-    }
-}
-
 function setCounter(uid, val) {
-    assertCounter(uid);
     users[uid].counter.value = val;
     saveUsers();
 }
-function incrementCounter(uid, val){
-    assertCounter(uid);
-    var current = users[uid].counter.value;
-    current += val;
-    users[uid].counter.value = current;
+function setCounterInterval(uid, interval){
+    users[uid].counter.interval = interval;
     saveUsers();
 }
-function setActiveMessageId(ctx){
-    assertCounter(uid);
-    users[uid].counter.activeMessageId // FIXME
-}
-function setCounterInterval(uid, val){
-    assertCounter(uid);
-    users[uid].counter.interval = val;
+function setActiveMessageId(uid, activeMessageId){
+    users[uid].counter.activeMessageId = activeMessageId;
     saveUsers();
 }
 function getCounter(uid) {
-    assertCounter(uid);
     return users[uid].counter.value;
 }
 function getCounterInterval(uid){
-    assertCounter(uid);
     return users[uid].counter.interval;
 }
 function getActiveMessageId(uid){
-    return users[uid].activeMessageId
+    return users[uid].counter.activeMessageId;
+}
 
-}
-function getChatType(uid) {
-    return users[uid].chat.type
-}
-function getAllCounters(uid) {
-    assertCounter(uid, '0');
-    return users[uid].counter;
+function getChatType(uid){
+    return users[uid].data.chat.type;
 }
 
 module.exports = {
@@ -143,11 +80,9 @@ module.exports = {
     getMetaData,
     setCounter,
     getCounter,
-    getAllCounters,
-    getActiveMessageId,
-    getCounterInterval,
-    incrementCounter,
-    setActiveMessageId,
     setCounterInterval,
-    getChatType
+    getCounterInterval,
+    getActiveMessageId,
+    getChatType,
+    setActiveMessageId
 };
